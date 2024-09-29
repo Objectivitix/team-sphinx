97d07db1e1e42f3f6972cec558415757e1933345
Ming Yu W <Ming@MingKundeMacBook-Pro.local>
2024-09-28 22:52:25 -0400

    ming ming

--- client/src/Join.tsx
+++ client/src/Join.tsx
@@ -6,37 +6,73 @@ export default function JoinPage() {
   const navigate = useNavigate();
 
   return (
-    <form style={{
-      width: "100%",
-      height: "100vh",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      flexDirection: "column",
-    }} onSubmit={(e) => {
-      const formData = new FormData(e.currentTarget)
+    <form
+      style={{
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+      onSubmit={(e) => {
+        const formData = new FormData(e.currentTarget);
 
-      let info: Record<string, string> = {};
+        let info: Record<string, string> = {};
 
-      for (const [key, value] of formData) {
-        if (value instanceof File) {
-          continue;
+        for (const [key, value] of formData) {
+          if (value instanceof File) {
+            continue;
+          }
+
+          info[key] = value;
         }
 
-        info[key] = value;
-      }
+        if ("username" in info && info.username && info.username.length > 0) {
+          join(info.username);
+          setOurName(info.username);
+          navigate("/lobby");
+        }
 
-      if ("username" in info && info.username && info.username.length > 0) {
-        join(info.username)
-        setOurName(info.username) 
-        navigate("/lobby")
-      }
-
-      e.preventDefault();
-    }}>
-      <h1>Pitchionary</h1>
-      <input name="username" placeholder="username"></input>
-      <button>submit</button>
+        e.preventDefault();
+      }}
+    >
+      {" "}
+      <link
+        href="https://fonts.googleapis.com/css?family=Itim"
+        rel="stylesheet"
+      ></link>
+      <img
+        src="public/icons/logo.png"
+        alt="Pitchionary"
+        style={{ width: "60vw", marginTop: "6vw", marginBottom: "4vw" }}
+      />
+      <input
+        name="username"
+        placeholder="Username"
+        style={{
+          width: "20vw",
+          height: "10vh",
+          borderRadius: "10px",
+          marginBottom: "2vw",
+          textAlign: "center",
+          fontSize: "calc(0.08 * 100vh)",
+          fontFamily: "Itim, sans-serif",
+        }}
+      ></input>
+      <button
+        style={{
+          width: "20vw",
+          height: "10vh",
+          borderRadius: "10px",
+          cursor: "pointer",
+          fontSize: "calc(0.08 * 100vh)",
+          padding: "0px 0px 5% 0px",
+          fontFamily: "Itim, sans-serif",
+        }}
+      >
+        Submit
+      </button>
     </form>
-  )
+  );
 }
